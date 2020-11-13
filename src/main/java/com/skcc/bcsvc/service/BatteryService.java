92806f7145339fde52ae2f56ce57bdf2051a09e9
jaehoon-k <publicdej@gmail.com>
2020-11-13 12:58:00 +0900

    Added rest api to transfer certificate token; PUT /battery/certificate

--- src/main/java/com/skcc/bcsvc/service/BatteryService.java
+++ src/main/java/com/skcc/bcsvc/service/BatteryService.java
@@ -9,6 +9,11 @@ import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
 public interface BatteryService {
+
+    String FUNC_ISSUE_CERTIFICATE = "issueCertificate";
+    String FUNC_VERIFY_CERTIFICATE = "verifyCertificate";
+    String FUNC_SAFE_TRANSFER_FROM = "safeTransferFrom";
     Map<String, Object> issueCertificate(String ownerAddr, String batteryId, String manufacturer, String modelNumber, BigInteger dateManufacture) throws IOException, TransactionException, CipherException;
     Map<String, Object> verifyCertificate(String ownerAddr, BigInteger tokenId, String tokenURI) throws ExecutionException, InterruptedException;
+    Map<String, Object> transferFrom(String fromAddr, String toAddr, BigInteger tokenId) throws IOException, TransactionException;
 }
